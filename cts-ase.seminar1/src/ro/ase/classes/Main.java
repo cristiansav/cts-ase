c1059a9fa5e773537aa4f368e458699b32c79288
cristiansav <75886279+cristiansav@users.noreply.github.com>
2022-05-22 18:31:42 +0300

    initial commit

---
+++ cts-ase.seminar1/src/ro/ase/classes/Main.java
@@ -0,0 +1,14 @@
+package ro.ase.classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {1, 2, 3, -2, -6, 7, 9, -5};
+		
+		System.out.println(Utils.countOccurMin(numbers));
+	}
+	
+	
+
+}
---
+++ cts-ase.seminar1/src/ro/ase/classes/Utils.java
@@ -0,0 +1,24 @@
+package ro.ase.classes;
+
+public class Utils {
+	
+	public static int countOccurMin(int[] array) {
+		int min = 0;
+		int occur = 0; 
+		for (int i = 0; i < array.length ; i++)
+		{
+			if(array[i] < min)
+			{
+				occur = 1;
+				min = array[i];
+			}
+			else if (array[i] == min)
+			{
+				occur++;
+			}
+		}
+		
+		return occur;
+	}
+	
+}
